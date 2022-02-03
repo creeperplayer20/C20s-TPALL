c81cadcbcb21eb65e5a08a15b4e76e90ccb0957c
creeperplayer20 <42560781+creeperplayer20@users.noreply.github.com>
2022-02-03 08:52:14 +0100

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace creeperplayer20\tpall;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Position;
+use pocketmine\command\Command;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+
+class Main extends PluginBase{
+
+public function onDisable() : void{
+    
+    $this->getLogger()->info("[§aC20§r - §6TPALL§r] §4Plugin is disabled!");
+        
+}
+
+public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool{
+
+    if($cmd->getName() == "tpall"){
+
+    foreach ($this->getServer()->getOnlinePlayers() as $tpPlayer) {
+
+        $tpPlayer->sendMessage("[§6TPALL§f] §gteleporting every player on the server to you!");
+        $tpPlayer->teleport($sender->getPosition());
+
+} return true;
+
+}}}
