fed2cb058f131b4db8b8431b860dcce22f04302f
creeperplayer20 <jdolezalek6@gmail.com>
2023-05-02 11:07:32 +0200

    :/

--- src/TpAll.php
+++ src/TpAll.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace creeperplayer20\tpall;
 
-use creeperplayer20\tpall\jojoe77777\FormAPI\CustomForm;
+use jojoe77777\FormAPI\CustomForm;
 use creeperplayer20\tpall\{Sound, Main, Functions};
 use creeperplayer20\tpall\task\TpAllTask;
 
@@ -34,7 +34,7 @@ class TpAll
         }
         $this->playersList[$player->getName()] = $playerList;
 
-        $form = new CustomForm(function (Player $player, $data) {
+        $tpAllForm = new CustomForm(function (Player $player, $data) {
             if ($data === null)
                 return true;
 
@@ -57,14 +57,14 @@ class TpAll
 
             Main::getInstance()->getServer()->broadcastMessage(Functions::getTeleportReason($data[4]));
         });
-        $form->setTitle(Functions::getConfigValue("ui-title"));
-        $form->addSlider("Seconds", 10, 300);
-        $form->addDropdown("Countdown sound:", ["Nothing", "random.levelup", "note.bass", "note.hat", "note.pling", "note.snare", "note.bd", "note.harp"]);
-        $form->addDropdown("Teleport sound:", ["Nothing", "random.levelup", "note.bass", "note.hat", "note.pling", "note.snare", "note.bd", "note.harp", "portal.travel"]);
-        $form->addDropdown("Teleport to:", $this->playersList[$player->getName()]);
-        $form->addInput("Reason:", "§6It's event time§f!");
+        $tpAllForm->setTitle(Functions::getConfigValue("ui-title"));
+        $tpAllForm->addSlider("Seconds", 10, 300);
+        $tpAllForm->addDropdown("Countdown sound:", ["Nothing", "random.levelup", "note.bass", "note.hat", "note.pling", "note.snare", "note.bd", "note.harp"]);
+        $tpAllForm->addDropdown("Teleport sound:", ["Nothing", "random.levelup", "note.bass", "note.hat", "note.pling", "note.snare", "note.bd", "note.harp", "portal.travel"]);
+        $tpAllForm->addDropdown("Teleport to:", $this->playersList[$player->getName()]);
+        $tpAllForm->addInput("Reason:", Functions::getConfigValue("reason-default"));
 
-        $player->sendForm($form);
+        $player->sendForm($tpAllForm);
     }
 
     public function TpAllLegacy(Player $player)
